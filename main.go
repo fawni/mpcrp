24eb16691f626bac695e08807e2a9f3762244277
obscurity <z@x4.pm>
2022-04-28 04:54:47 +0200

    refactor: move `types.go` to `main.go`

--- main.go
+++ main.go
@@ -16,6 +16,36 @@ import (
 	"github.com/x6r/rp/rpc"
 )
 
+type state int8
+
+type playback struct {
+	file           string
+	state          state
+	statestring    string
+	position       int
+	duration       int
+	durationstring string
+	version        string
+
+	// filepatharg string
+	// filepath    string
+	// filedirarg  string
+	// filedir     string
+	// positionstring string
+	// volumelevel    int
+	// muted          bool
+	// playbackrate   float32
+	// size           string
+	// reloadtime     int
+}
+
+const (
+	idling state = iota - 1
+	stopped
+	paused
+	playing
+)
+
 var (
 	c  *rpc.Client
 	pb playback
